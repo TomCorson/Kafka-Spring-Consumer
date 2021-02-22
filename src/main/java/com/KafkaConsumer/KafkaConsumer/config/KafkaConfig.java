307ae8bd0e5ba05dd7ca4e40b90e7a3746113afb
TomCorson <corsontom@yahoo.com>
2021-02-22 10:31:35 -0500

    default constructor user class

--- src/main/java/com/KafkaConsumer/KafkaConsumer/config/KafkaConfig.java
+++ src/main/java/com/KafkaConsumer/KafkaConsumer/config/KafkaConfig.java
@@ -1,5 +1,6 @@
 package com.KafkaConsumer.KafkaConsumer.config;
 
+import com.KafkaConsumer.KafkaConsumer.model.User;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.context.annotation.Bean;
@@ -8,6 +9,7 @@ import org.springframework.kafka.annotation.EnableKafka;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.ConsumerFactory;
 import org.springframework.kafka.core.DefaultKafkaConsumerFactory;
+import org.springframework.kafka.support.serializer.JsonDeserializer;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -31,4 +33,30 @@ public class KafkaConfig {
         factory.setConsumerFactory(consumerFactory());
         return factory;
     }
+    @Bean
+    public ConsumerFactory<String, User> userConsumerFactory(){
+        Map<String,Object> config = new HashMap<>();
+        config.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG,"127.0.0.1:9092");
+        config.put(ConsumerConfig.GROUP_ID_CONFIG,"group_json");
+        config.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
+        config.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
+        return new DefaultKafkaConsumerFactory<String,User>(config, new StringDeserializer(),new JsonDeserializer<>(User.class));
+    }
+    @Bean
+    public ConcurrentKafkaListenerContainerFactory<String, User> userKafkaListenerFactory(){
+        ConcurrentKafkaListenerContainerFactory<String, User> factory = new ConcurrentKafkaListenerContainerFactory();
+        factory.setConsumerFactory(userConsumerFactory());
+        return factory;
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/KafkaConsumer/KafkaConsumer/listener/KafkaConsumer.java
+++ src/main/java/com/KafkaConsumer/KafkaConsumer/listener/KafkaConsumer.java
@@ -1,5 +1,6 @@
 package com.KafkaConsumer.KafkaConsumer.listener;
 
+import com.KafkaConsumer.KafkaConsumer.model.User;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
@@ -9,5 +10,9 @@ public class KafkaConsumer {
     @KafkaListener(topics = "Kafka_Example", groupId = "group_id")
     public void consume(String message){
         System.out.println("Consumed message: " + message);
+    }
+    @KafkaListener(topics = "Kafka_Example_json", groupId = "group_json", containerFactory = "userKafkaListenerFactory")
+    public void consumeJson(User user){
+        System.out.println("Consumed Json: " + user);
     }
 }
